f662ea13d1fffc281c0c2817d89f268769c9749d
Yusuf Arslan <hello@yusufarslan.dev>
2024-02-16 01:29:30 +0300

    shippers updated

--- config.php
+++ config.php
@@ -23,7 +23,7 @@ return array(
             "logo" => "assets/logos/mng.png"
         ),
         "horoz" => array(
-            "company" => "Horoz Kargo",
+            "company" => "Horoz Lojistik",
             "url" => "https://app3.horoz.com.tr/wsKurumsal/_genel/frmGonderiTakip.aspx?lng=tr",
             "logo" => "assets/logos/horoz.png"
         ),
@@ -43,34 +43,19 @@ return array(
             "logo" => "assets/logos/filo.png"
         ),
         "tnt" => array(
-            "company" => "TNT Kargo",
+            "company" => "TNT Express",
             "url" => "https://www.tnt.com/express/tr_tr/site/shipping-tools/tracking.html?searchType=con&cons=",
             "logo" => "assets/logos/tnt.png"
         ),
         "dhl" => array(
-            "company" => "DHL Kargo",
+            "company" => "DHL",
             "url" => "https://www.dhl.com/tr-tr/home/tracking.html?tracking-id=",
             "logo" => "assets/logos/dhl.png"
         ),
         "fedex" => array(
-            "company" => "Fedex Kargo",
+            "company" => "FedEx",
             "url" => "https://www.fedex.com/fedextrack/?action=track&tracknumbers=",
             "logo" => "assets/logos/fedex.png"
-        ),
-        "foodman" => array(
-            "company" => "FoodMan Kargo",
-            "url" => "https://www.foodman.online/GonderiSorgu.aspx?gonderino=",
-            "logo" => "assets/logos/foodman.png"
-        ),
-        "postman" => array(
-            "company" => "Postman Kargo",
-            "url" => "http://85.99.122.231/hareket.asp?har_kod=",
-            "logo" => "assets/logos/postman.png"
-        ),
-        "iyi" => array(
-            "company" => "İyi Kargo",
-            "url" => "https://www.geowix.com/kargom-nerede?tracking_code=",
-            "logo" => "assets/logos/iyi.png"
         ),
         "tex" => array(
             "company" => "Trendyol Express",
@@ -78,20 +63,19 @@ return array(
             "logo" => "assets/logos/tex.png"
         ),
         "hepsijet" => array(
-            "company" => "HepsiJET",
+            "company" => "hepsiJET",
             "url" => "https://www.hepsijet.com/gonderi-takibi/",
             "logo" => "assets/logos/hepsijet.png"
         ),
         "Sendeo" => array(
             "company" => "Sendeo Kargo",
-            "url" => "https://www.sendeo.com.tr/shipment-tracking/",
+            "url" => "https://sendeo.com.tr/tracking",
             "logo" => "assets/logos/sendeo.png"
         ),
         "carrtell" => array(
-            "company" => "Carrtell Kargo",
+            "company" => "Carrtell",
             "url" => "https://takip.carrtell.co/",
             "logo" => "assets/logos/carrtell.png"
         ),
-        
     )
 );
